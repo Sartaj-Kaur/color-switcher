c44dfca43e4fc89ba1d318248cd722b75ebf2acb
Sartaj Kaur <sartajkaursandhu@gmail.com>
2025-11-22 11:33:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function  changeColor(color){
+    document.body.style.backgroundColor = color;
+}
+
+// == comparing equality
+// === comparing in addition to comparing their data types
+
+// console.log(type.of)
+
+console.log("Evolve AI")
